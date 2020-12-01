46c83faa90169328be0838a63a056e407d13d35f
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2020-12-01 09:31:42 -0300

    Ok

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,21 +1,20 @@
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAEuF48liaoXkQHhTZEBEn-_XLXg475eSk",
-    authDomain: "biblia-sagrada-arc.firebaseapp.com",
-    databaseURL: "https://biblia-sagrada-arc.firebaseio.com",
-    projectId: "biblia-sagrada-arc",
-    storageBucket: "biblia-sagrada-arc.appspot.com",
-    messagingSenderId: "97461857014",
-    appId: "1:97461857014:web:4b282536c62c9e78299ad7",
-    measurementId: "G-E02LPSLJZ8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBayBLRc2DBNwwjG-onz5frwp-ulLZK7b0",
+  authDomain: "biblia-sagrada-almeida-908d3.firebaseapp.com",
+  databaseURL: "https://biblia-sagrada-almeida-908d3.firebaseio.com",
+  projectId: "biblia-sagrada-almeida-908d3",
+  storageBucket: "biblia-sagrada-almeida-908d3.appspot.com",
+  messagingSenderId: "523477231521",
+  appId: "1:523477231521:web:0ba38dc311f041828f0de0",
+  measurementId: "G-9M1Y16LY1C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  firebase.auth().signInAnonymously().catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-  });
+firebase.auth().signInAnonymously().catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorMessage)
+});
